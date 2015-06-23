c0969eaf905bb241ee0422b372b62bae7f497ac9
Marek Ciupak <ciupakm@gmail.com>
2015-06-24 01:29:42 +0200

    Validation for matches.

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,4 +1,15 @@
 class Match < ActiveRecord::Base
-  belongs_to :winner, :class_name => "Player"
-  belongs_to :loser, :class_name => "Player"
+ 	belongs_to :winner, :class_name => "Player"
+ 	belongs_to :loser, :class_name => "Player"
+
+	validates :winner, :loser, :date, presence: true
+	validates :loser_score, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than: 10 }
+
+	validate :in_a_single_match_can_not_be_the_same_two_players
+
+	def in_a_single_match_can_not_be_the_same_two_players
+	    if winner && winner == loser
+	      errors.add(:loser, "In a single match can't be the same two players")
+    	end
+	end
 end
