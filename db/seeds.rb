2a2939f6ecd90f7fffa7743039b7670e93248c6f
Marek Ciupak <ciupakm@gmail.com>
2015-06-25 20:09:10 +0200

    Added seeds data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+p = Array.new
+
+p.push(Player.create(firstname: 'Daenerys', lastname: 'Targaryen', email: 'daenerys.targaryen@example.localhost'))
+p.push(Player.create(firstname: 'Jon', lastname: 'Snow', email: 'jon.snow@example.localhost'))
+p.push(Player.create(firstname: 'Arya', lastname: 'Stark', email: 'arya.stark@example.localhost'))
+p.push(Player.create(firstname: 'Sansa', lastname: 'Stark', email: 'sansa.stark@example.localhost'))
+p.push(Player.create(firstname: 'Tyrion', lastname: 'Lannister', email: 'tyrion.lannister@example.localhost'))
+
+100.times {
+	Match.create(
+		date: rand(2.months).seconds.ago, 
+		winner: winner = p.sample, 
+		loser: p.reject{|p| p == winner}.sample, 
+		loser_score: rand(0..9)
+	)
+}
